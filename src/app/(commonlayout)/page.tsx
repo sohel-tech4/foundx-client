96c728638b332243770a3f3ed13bf4f7322efa9b
sohel-tech4 <soheltech4@gmail.com>
2025-02-09 08:23:42 +0600

    add

--- src/app/(commonlayout)/page.tsx
+++ src/app/(commonlayout)/page.tsx
@@ -1,11 +1,7 @@
-import { Link } from "@heroui/link";
 import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
 
-import { siteConfig } from "@/src/config/site";
 import { title, subtitle } from "@/src/components/primitives";
-import { GithubIcon } from "@/src/components/icons";
 
 export default function Home() {
   return (
@@ -22,27 +18,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
+      <div className="flex gap-3"></div>
 
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
